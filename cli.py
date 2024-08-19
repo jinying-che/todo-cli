05f500a3590d34e705eeaf40d66ce3d081dbb910
Jinying Che <chejinying@me.com>
2024-08-19 23:47:38 +0800

    init cli

---
+++ cli.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import click
+
+@click.command()
+@click.option('--count', default=1, help='number of greetings')
+@click.argument('name')
+def hello(count, name):
+    for x in range(count):
+        click.echo(f"Hello {name}!")
+
+if __name__ == '__main__':
+    hello()
